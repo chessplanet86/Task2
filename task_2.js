4fd83bb54158342a2f018faf9fb455b2488a133b
chessplanet86 <49858508+chessplanet86@users.noreply.github.com>
2019-05-29 14:27:33 +0500

    Add files via upload

---
+++ task_2.js
@@ -0,0 +1,58 @@
+var app = new Vue({
+        el: "#app",
+        data() {
+            return {
+                column: '',
+                row: '',
+                loc: 'D3',
+                allowable: [],
+                conclusion: ''
+            }
+        },
+        methods: {
+            coordinates() {
+                var newColumn = 0;
+                var newRow = '';
+                this.column = this.loc[0]; // координата по вертикали (буква A, В, C, D, E, F, G, H)
+                this.row = Number(this.loc[1]); // Координата по горизонтали (цифра от 1 до 8)
+                var A = this.loc[0].charCodeAt(0); // Получаем код симовла, но можно и без этого.
+                this.allowable.length = 0; // обнуляем массив, чтобы каждый раз выводились только варианты для каждой клетки и не было мусора
+                this.conclusion = ''; // обнуляем строку, чтобы вывод был чистым и не засорялся
+                for (var i = 1; i < 3; i++) {
+                    //Условие на поиск нижних возможных клеток, куда может попасть конь
+                    if (this.row - i > 0) {
+                        newRow = this.row - i;
+                        if ((A - (3 - i)) > 64) {
+                            newColumn = A - (3 - i);
+                            this.allowable.push(String.fromCharCode(newColumn) + newRow);
+                        }
+                        if ((A + (3 - i)) < 73) {
+                            newColumn = A + (3 - i);
+                            this.allowable.push(String.fromCharCode(newColumn) + newRow);
+                        }
+                    }
+                    if (this.row + i < 9) {
+                        newRow = this.row + i;
+
+                        if ((A - (3 - i)) > 64) {
+                            newColumn = A - (3 - i);
+
+                            this.allowable.push(String.fromCharCode(newColumn) + newRow);
+                        }
+
+                        if ((A + (3 - i)) < 73) {
+                            newColumn = A + (3 - i);
+
+                            this.allowable.push(String.fromCharCode(newColumn) + newRow);
+                        }
+                    }
+                }
+
+                this.allowable.forEach(element => {
+                    this.conclusion += element + ' ';
+                });
+
+                alert('Возможные варианты хода: ' + this.conclusion);
+            }
+        }
+    })
