f178109461a1794d5e09220c11f03396e2f6f442
Юсуф Алексей А <a_yusuf@ugrasu.ru>
2019-06-03 11:01:50 +0500

    Доработал логику с проверками

--- task_2.js
+++ task_2.js
@@ -1,20 +1,23 @@
 var app = new Vue({
-        el: "#app",
-        data() {
-            return {
-                column: '',
-                row: '',
-                loc: 'D3',
-                allowable: [],
-                conclusion: ''
-            }
-        },
-        methods: {
-            coordinates() {
-                var newColumn = 0;
-                var newRow = '';
-                this.column = this.loc[0]; // координата по вертикали (буква A, В, C, D, E, F, G, H)
-                this.row = Number(this.loc[1]); // Координата по горизонтали (цифра от 1 до 8)
+    el: "#app",
+    data() {
+        return {
+            column: '',
+            row: '',
+            loc: 'D3',
+            allowable: [],
+            conclusion: '',
+            error: false
+        }
+    },
+    methods: {
+        coordinates() {
+            this.error = false;
+            var newColumn = 0;
+            var newRow = '';
+            this.column = this.loc[0]; // координата по вертикали (буква A, В, C, D, E, F, G, H)
+            this.row = Number(this.loc[1]); // Координата по горизонтали (цифра от 1 до 8)
+            if (this.loc[0] >= 'A' && this.loc[0] <= 'H' && this.row > 0 && this.row < 9) {
                 var A = this.loc[0].charCodeAt(0); // Получаем код симовла, но можно и без этого.
                 this.allowable.length = 0; // обнуляем массив, чтобы каждый раз выводились только варианты для каждой клетки и не было мусора
                 this.conclusion = ''; // обнуляем строку, чтобы вывод был чистым и не засорялся
@@ -53,6 +56,14 @@ var app = new Vue({
                 });
 
                 alert('Возможные варианты хода: ' + this.conclusion);
+
+            } else {
+                
+                this.error = true;
+                this.notification = 'Вы ввели: '+ this.loc+'\nПервая координата должна быть латинской буквой (Возможные варианты: A, B, C, D, E, F, G, H).\nВторая координата должна быть цифрой от 1 до 8.';
+                this.conclusion = 'Ошибка'
+                this.loc='';
             }
         }
-    })
+    }
+})
